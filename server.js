9ddf30bf75ea600fcfbab9364e832a52d85e38a7
Jure Podgoršek <podgorsek.jurij@gmail.com>
2017-08-04 14:52:41 +0000

    dodan podurl

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/sz3', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/three.min.js', function(req, res){
+app.get('/sz3/three.min.js', function(req, res){
     res.sendFile(__dirname + '/three.min.js');
 });
 
-app.get('/anim.js', function(req, res){
+app.get('/sz3/anim.js', function(req, res){
     res.sendFile(__dirname + '/anim.js');
+});
+
+app.get('/sz3/socket.io.js', function(req, res){
+    res.sendFile(__dirname + '/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js');
 });
 
 io.on('connection', function(socket){
